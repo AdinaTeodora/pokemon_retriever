fc5b603914697f2d2799eeda29c4665d49bb6703
Adina Teodora <teodora_ardeleanu@yahoo.com>
2022-03-13 20:53:29 +0000

    Added pokemon fetch functions and respective unit tests/config

--- lib/pokemon_retriever.ex
+++ lib/pokemon_retriever.ex
@@ -1,18 +1,41 @@
 defmodule PokemonRetriever do
-  @moduledoc """
-  Documentation for `PokemonRetriever`.
-  """
+  use Tesla
+
+  plug Tesla.Middleware.JSON
+
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PokemonRetriever.hello()
-      :world
-
+      iex> PokemonRetriever.fetch_pokemon_name(1)
+           "bulbasaur"
   """
-  def hello do
-    :world
+  def fetch_pokemon_name(id) do
+    case get("https://pokeapi.co/api/v2/pokemon/#{id}") do
+      {:ok, %Tesla.Env{status: 200, body: %{"name" => name}}} ->
+        name
+      {:ok, %Tesla.Env{status: 404}} ->
+        "not found"
+      _ ->
+        "unknown error"
+    end
   end
+
+  @doc """
+      iex> PokemonRetriever.fetch_pokemon_id("bulbasaur")
+           1
+  """
+  def fetch_pokemon_id(name) do
+    case get("https://pokeapi.co/api/v2/pokemon/#{name}") do
+      {:ok, %Tesla.Env{status: 200, body: %{"id" => id}}} ->
+        id
+      {:ok, %Tesla.Env{status: 404}} ->
+        "not found"
+      _ ->
+        "unknown error"
+    end
+  end
+
 end
+
+# to run:
+#   iex -S mix
+#   c("lib/pokemon_retriever.ex")
--- test/pokemon_retriever_test.exs
+++ test/pokemon_retriever_test.exs
@@ -1,8 +1,40 @@
 defmodule PokemonRetrieverTest do
   use ExUnit.Case
-  doctest PokemonRetriever
 
-  test "greets the world" do
-    assert PokemonRetriever.hello() == :world
+  import Tesla.Mock
+
+  # Successful test cases
+  test "success pokemon name fetch" do
+    mock(fn
+      %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/1"} ->
+        %Tesla.Env{status: 200, body: %{"name" => "bulbasaur"}}
+    end)
+    assert PokemonRetriever.fetch_pokemon_name(1) == "bulbasaur"
   end
-end
+
+  test "success pokemon id fetch" do
+    mock(fn
+      %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/bulbasaur"} ->
+        %Tesla.Env{status: 200, body: %{"id" => 1}}
+    end)
+    assert PokemonRetriever.fetch_pokemon_id("bulbasaur") == 1
+  end
+
+  # Error test cases
+  test "not found pokemon name fetch" do
+    mock(fn
+      %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/10000"} ->
+        %Tesla.Env{status: 404}
+    end)
+    assert PokemonRetriever.fetch_pokemon_name(10000) == "not found"
+  end
+
+  test "not found pokemon id fetch" do
+    mock(fn
+      %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/bulbasaur12"} ->
+        %Tesla.Env{status: 404}
+    end)
+    assert PokemonRetriever.fetch_pokemon_id("bulbasaur12") == "not found"
+  end
+
+  end
